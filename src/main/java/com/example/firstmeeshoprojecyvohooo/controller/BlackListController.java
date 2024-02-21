00c3fcb2a7b08e0d51902a0379f5d43fcb7c0af8
Palak Agarwal <palakagarwal@P6FFP6F4R0.local>
2024-02-21 12:37:18 +0530

    new changes

--- src/main/java/com/example/firstmeeshoprojecyvohooo/controller/BlackListController.java
+++ src/main/java/com/example/firstmeeshoprojecyvohooo/controller/BlackListController.java
@@ -3,11 +3,13 @@ package com.example.firstmeeshoprojecyvohooo.controller;
 
 import com.example.firstmeeshoprojecyvohooo.dto.BlackListRequestDto;
 import com.example.firstmeeshoprojecyvohooo.service.BlackListService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class BlackListController {
 
     @Autowired
@@ -16,6 +18,7 @@ public class BlackListController {
     @PostMapping("/v1/blacklist")
     public ResponseEntity<Object> blacklistPhoneNumbers(@RequestBody BlackListRequestDto requestDto)
     {
+        log.info("request to blaclist phone numbers : {}", requestDto)
         return blackListService.blacklistPhoneNumbers(requestDto);
     }
     @PutMapping("/v1/blacklist")
